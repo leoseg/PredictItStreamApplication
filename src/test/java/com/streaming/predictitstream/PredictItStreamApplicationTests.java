03257dda38b02d66f5e71cea8f30c9ee6798daa2
lseeger <lseeger@inovex.de>
2022-01-13 18:18:19 +0100

    added tests TODO implement consumertest for kafka consumer

--- src/test/java/com/streaming/predictitstream/PredictItStreamApplicationTests.java
+++ src/test/java/com/streaming/predictitstream/PredictItStreamApplicationTests.java
@@ -1,13 +1,24 @@
 package com.streaming.predictitstream;
 
+import com.streaming.predictitstream.services.candidateServices.ContractService;
 import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PredictItStreamApplicationTests {
 
+    @Autowired
+    ContractService contractService;
+
+
+
+
+
     @Test
     void contextLoads() {
+        assertNotNull(contractService);
     }
 
 }
---
+++ src/test/java/com/streaming/predictitstream/kafkaConsumer/PredictItConsumerTests.java
@@ -0,0 +1,36 @@
+package com.streaming.predictitstream.kafkaConsumer;
+
+import com.streaming.predictitstream.services.candidateServices.ContractService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
+
+
+@SpringBootTest
+@DirtiesContext
+@EmbeddedKafka(partitions = 1 ,bootstrapServersProperty = "localhost:9092"
+        ,brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" }
+        ,topics={"president.test"})
+public class PredictItConsumerTests {
+
+    @MockBean
+    ContractService contractService;
+
+    @Autowired
+    private KafkaTemplate<String,String> kafkaTemplate;
+
+    @Autowired
+    private PredictItConsumer predictItConsumer;
+
+    @Test
+    public void testListenPresidentTopics() throws Exception{
+        kafkaTemplate.send("president.test","somemssage");
+
+    }
+
+
+}
