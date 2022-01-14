c3e1b0f941acdf93c91981b1dc8fc0261ae6644e
lseeger <lseeger@inovex.de>
2022-01-14 15:48:08 +0100

    added user variables in deploy scripts

--- bashscripts/deploy_local.sh
+++ bashscripts/deploy_local.sh
@@ -2,8 +2,8 @@
 export KAFKAADRESS="kafka:29092"
 cd .. &&
 ./gradlew build &&
-docker build . -f DockerfileConnect -t seeger16/connectimage &&
-docker build . -t seeger16/predictitconsumer &&
+docker build . -f DockerfileConnect -t connectimage &&
+docker build . -t predictitconsumer &&
 docker-compose -f docker-compose-local.yml up --force-recreate --renew-anon-volumes -d &&
 docker exec predictitstream_kafka_1 cub kafka-ready -b localhost:9092 1 20
 cd bashscripts &&
