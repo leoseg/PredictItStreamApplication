ea42fccc8e0c68a24023a22c02e93fbf253242b3
lseeger <lseeger@inovex.de>
2022-01-14 12:03:46 +0100

    created configuration and updated docker-compose file for local deployment

--- bashscripts/deploy_local.sh
+++ bashscripts/deploy_local.sh
@@ -2,9 +2,9 @@
 export KAFKAADRESS="kafka:29092"
 cd .. &&
 ./gradlew build &&
-docker build . -f DockerfileConnect -t connectimage &&
-docker build . -t predictitconsumer &&
-docker-compose up --force-recreate --renew-anon-volumes -d &&
+docker build . -f DockerfileConnect -t seeger16/connectimage &&
+docker build . -t seeger16/predictitconsumer &&
+docker-compose -f docker-compose-local.yml up --force-recreate --renew-anon-volumes -d &&
 docker exec predictitstream_kafka_1 cub kafka-ready -b localhost:9092 1 20
 cd bashscripts &&
 sh ./connect_predictit.sh
